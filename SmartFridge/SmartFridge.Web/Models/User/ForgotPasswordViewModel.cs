d8534107b01b2398aeaaec2afdb5dba54bc8c113
Jp Doohan <johnpauldoohan@gmail.com>
2025-06-18 08:10:24 +0100

    Initial commit

---
+++ SmartFridge/SmartFridge.Web/Models/User/ForgotPasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartFridge.Web.Models.User;
+public class ForgotPasswordViewModel
+{
+    [Required]
+    public string Email { get; set; }
+    
+}
---
+++ SmartFridge/SmartFridge.Web/Models/User/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using SmartFridge.Data.Entities;
+
+namespace SmartFridge.Web.Models.User;
+public class ProfileViewModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Remote(action: "VerifyEmailAvailable", controller: "User", AdditionalFields = nameof(Id))]
+    public string Email { get; set; }
+
+    public Role Role { get; set; }
+
+}
